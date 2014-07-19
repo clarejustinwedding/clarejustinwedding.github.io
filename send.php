d2b2d0d93cf3357c5bb4c721652ec22db66cf27e
clarejustwedding <justinlodd@gmail.com>
2014-07-20 01:02:58 +1000

    more changes

--- send.php
+++ send.php
@@ -1,5 +1,5 @@
 <?php
-$YourEmailAddress = "danielodd@gmail.com;d.odd@nib.com.au"; //enter your e-mail address here, ex. $YourEmailAddress = "info@pehaa.com";
+$YourEmailAddress = "danielodd@gmail.com"; //enter your e-mail address here, ex. $YourEmailAddress = "info@pehaa.com";
 
 //  SEND
 //
